2ba5dd118e07aa00678bb8f2004f1b26997171db
Deni <denizakya@gmail.com>
2020-10-17 22:09:43 +0700

    Init project

---
+++ airQualityTests/AirVisualApiTest.swift
@@ -0,0 +1,19 @@
+//
+//  AirVisualApiTest.swift
+//  airQualityTests
+//
+//  Created by Deni Zakya on 17/10/20.
+//
+
+import XCTest
+
+import RxSwift
+import RxCocoa
+
+class AirVisualApiTest: XCTestCase {
+
+    func testGetCountries() {
+        
+    }
+
+}
