10223488ab6a26cd785ba2df80e3074df4106bf8
Deni <denizakya@gmail.com>
2020-10-17 22:00:30 +0700

    Initial Commit

---
+++ airQuality/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  airQuality
+//
+//  Created by Deni Zakya on 17/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
