a4cd48de4981d827ecac81502512c3ea54bb1ca0
Deni <denizakya@gmail.com>
2020-10-18 20:06:34 +0700

    Show nearest city data in main view

---
+++ airQuality/MainViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MainViewController.swift
+//  airQuality
+//
+//  Created by Deni Zakya on 18/10/20.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import os.log
+
+final class MainViewController: UIViewController {
+    
+    @IBOutlet weak var aqiUSLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var weatherIconView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    private let disposeBag = DisposeBag()
+    private let api = AirVisualApi()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let cityData = api.getNearestCityData()
+            .asDriver(onErrorDriveWith: .empty())
+
+        cityData.map { $0.city }
+            .drive(cityLabel.rx.text)
+            .disposed(by: disposeBag)
+        cityData.map { "\($0.state), \($0.country)" }
+            .drive(originLabel.rx.text)
+            .disposed(by: disposeBag)
+        cityData.map(\.pollution.airQualityIndexUS).map { "\($0)" }
+            .drive(aqiUSLabel.rx.text)
+            .disposed(by: disposeBag)
+        cityData.map(\.weather.temperature).map { "\($0)°C" }
+            .drive(temperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        cityData.map(\.weather.icon)
+            .compactMap { URL(string: "https://www.airvisual.com/images/\($0).png") }
+            .compactMap { getData(of: $0) }.map { UIImage(data: $0) }
+            .drive(weatherIconView.rx.image)
+            .disposed(by: disposeBag)
+    }
+
+}
+
+private func getData(of url: URL) -> Data? {
+    do {
+        return try Data(contentsOf: url)
+    } catch {
+        os_log(.error, "unable to retrieve data")
+        return nil
+    }
+}
